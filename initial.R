1a6b5ba960965ad5cfe2d6bc8f150656e8bc91d4
tobyspocket <tobydiethelm@gmail.com>
2020-11-01 12:15:06 +0100

    Laden der Packages ersetzt

--- initial.R
+++ initial.R
@@ -1,15 +1,16 @@
 #### INITIAL LOAD #######################################################
 
-# Packages
-library(pdftools)
-library(tidyverse)
-library(dplyr)
-library(googlesheets4)
-library(rvest)
-library(shiny)
-library(ggplot2)
-library(RSwissMaps)
-library(plotly)
+# Package names
+packages <- c("ggplot2", "dplyr", "tidyverse", "pdftools", "shiny", "googlesheets4", "rvest", "RSwissMaps", "plotly")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
 
 
 
